7bb5c771e00b917fb2b9c734d27fb8c50760b60e
anh-ton <nguyenletoan00@gmail.com>
2026-06-11 23:35:41 +0700

    add snap and zalo

--- install.sh
+++ install.sh
@@ -7,3 +7,18 @@ ibus-daemon -drx
 
 #install microsoft font
 sudo apt install ttf-mscorefonts-installer -y
+
+#install onlyoffice
+sudo apt install snapd -y
+sudo systemctl start --now snapd
+sudo systemctl enable --now snapd
+sudo systemctl start --now snapd.apparmor
+sudo systemctl enable --now snapd.apparmor
+sudo snap install onlyoffice-desktopeditors
+
+#install zalo
+mkdir -p ~/Applications
+cd ~/Applications
+git clone https://github.com/realdtn2/zalo-linux-2026.git
+cd zalo-linux-2026
+./install.sh
